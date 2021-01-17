ab3d614136113d815b55e4249a71b37e2c65549f
Shaance <christophe.ha1@gmail.com>
2021-01-17 17:44:30 +0100

    Fix issue with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   rules: {
     "import/extensions" : 0,
     "@typescript-eslint/no-use-before-define": 0,
+    "@typescript-eslint/camelcase": 0,
+    "@typescript-eslint/no-unused-vars": 0,
     "max-len": 1
   }
 };
